850921ec6b4888b01cec014b5af2214cf382f604
migunasekera <luminapursuit@DESKTOP-79L63OQ.localdomain>
2019-05-30 23:19:27 -0400

    Sharing work that I have already done. Currently issues with properly mapping the robot using the Wheel odometry.

---
+++ test_load_data.py
@@ -0,0 +1,11 @@
+# Jinwook Huh
+
+import load_data as ld
+import p3_util as ut
+
+acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, imu_ts	 = ld.get_imu('imuRaw20')
+
+FR, FL, RR, RL,enc_ts = ld.get_encoder('Encoders20')
+
+lidar = ld.get_lidar('Hokuyo20')
+ut.replay_lidar(lidar)
